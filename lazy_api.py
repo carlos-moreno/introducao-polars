9f9667ee29728531a67ce774321f8f8b7364b36d
Carlos Moreno <omorenodovale@gmail.com>
2024-02-04 07:34:59 -0300

    feat: Demonstrando os 5 primeiros e os 5 últimos registros head() e tail()

--- lazy_api.py
+++ lazy_api.py
@@ -2,4 +2,12 @@ import polars as pl
 
 q1 = pl.scan_csv("assets/csv_exemplo.csv", separator=";").collect(streaming=True)
 
+df = pl.read_csv("assets/csv_exemplo.csv", separator=";")
+
+print(df)
+
+print(df.head())
+
+print(df.tail())
+
 print(q1)
