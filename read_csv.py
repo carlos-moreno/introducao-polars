bf0f884732eae8d251226770e88a5c7389530e16
carlos-moreno <omorenodovale@gmail.com>
2024-02-09 21:53:41 -0300

    Alter head and tail

--- read_csv.py
+++ read_csv.py
@@ -5,6 +5,6 @@ df = pl.read_csv("assets/csv_exemplo.csv", separator=";")
 
 print(f"Colunas do Dataframe => {df.columns}")
 
-print(df.head())
+print(df.head(4))
 
-print(df.tail(3))
+print(df.tail(2))
